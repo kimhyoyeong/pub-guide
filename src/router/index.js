07f536637dc88d1208f28b2866af6110b491760a
hyo.kim <36067398+kimhyoyeong@users.noreply.github.com>
2024-05-10 14:22:44 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -6,25 +6,25 @@ const routes = [
     path: "/",
     name: "index",
     component: Index,
-    //props: parseActiveIndex,
+    props: parseActiveIndex,
   },
   {
     path: "/page1",
     name: "page1",
     component: () => import("@/views/page1.vue"),
-    //props: parseActiveIndex,
+    props: parseActiveIndex,
   },
   {
     path: "/page2",
     name: "page2",
     component: () => import("@/views/page2.vue"),
-    //props: parseActiveIndex,
+    props: parseActiveIndex,
   },
   {
     path: "/page3",
     name: "page3",
     component: () => import("@/views/page3.vue"),
-    //props: parseActiveIndex,
+    props: parseActiveIndex,
   },
 ];
 
@@ -34,8 +34,8 @@ const router = createRouter({
 });
 
 //주소 표시줄에서 activeIndex 값을 읽어와서 페이지에 전달
-/* function parseActiveIndex(route) {
+function parseActiveIndex(route) {
   return { activeIndex: parseInt(route.query.activeIndex) || 0 };
-} */
+}
 
 export default router;
