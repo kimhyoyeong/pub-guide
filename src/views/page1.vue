2da446240df9faadc632cbbc255b5831d53d07ac
hyo.kim <36067398+kimhyoyeong@users.noreply.github.com>
2024-05-13 13:39:57 +0900

    update

--- src/views/page1.vue
+++ src/views/page1.vue
@@ -5,8 +5,8 @@
         <h3 class="text-h6 q-mb-none">1) alt 제공</h3>
       </q-card-section>
 
-      <q-img src="../images/@test.png" :ratio="16 / 9" fit="contain">
-        <div class="absolute-top bg-primary">준수 사례</div>
+      <q-img src="images/@test.png" :ratio="16 / 9" fit="contain">
+        <div class="absolute-top bg-primary">11준수 사례</div>
       </q-img>
 
       <q-card-section> alt에는 적절한 대체 텍스트를 제공한다. </q-card-section>
